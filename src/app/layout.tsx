ac2201d6853aefcb241ed6dd8d98598886607031
Ghost-web-ops <omaryasser011549@gmail.com>
2025-07-29 06:14:26 +0300

    Initial commit: Complete movie finder application

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,12 @@ import Footer from "@/components/Footer";
 
 
 export const metadata: Metadata = {
+   metadataBase: new URL('https://movie-finder-app-nextjs.vercel.app'),
   title: "MovieFinder - Discover & Search Your Favorite Films",
   description: "Explore a vast library of films with MovieFinder. Get details on the most popular movies, search for any title, and discover hidden gems. Your ultimate guide to the world of cinema, built with Next.js and the TMDB API.",
+    alternates: {
+    canonical: '/',
+  },
 };
 
 export default function RootLayout({
